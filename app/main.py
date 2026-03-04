511926fafc189bc444d8b736bf020333de2bdf0b
Alex <alexgomez2202@gmail.com>
2026-03-03 23:38:14 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -92,7 +92,7 @@ def main():
             path = get_path(cmd)
             if path:
                 res = subprocess.run([cmd, *args], capture_output=True, text=True)
-                sys.stdout.write(res)
+                sys.stdout.write(res.stdout)
             else:   
                 sys.stdout.write(f"{cmd}: command not found \n")
     
